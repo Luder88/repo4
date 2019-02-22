a99ded976257b7925c01f0b5dc591da661f65a98
user <user@qq.com>
2019-02-23 00:06:07 +0800

    2

--- FirstClass.java
+++ FirstClass.java
@@ -4,4 +4,7 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("First Class...");
     }
+    public void t(){
+        System.out.println("11");
+    }
 }
